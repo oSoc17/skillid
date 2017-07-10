07aab90b5f9413dec4db4cb107feb15100848bb6
evajacobs <eva.jacobs@student.howest.be>
2017-07-10 11:31:16 +0200

    skillid structure

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <div id="app">
+    <h1>Skillid</h1>
+  </div>
+</template>
+
+<script src="./js/app.js"></script>
+
+<style src="./css/app.css"></style>
---
+++ src/js/app.js
@@ -0,0 +1,8 @@
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
