2b231eacf8102201f64d31beb87e99e1e7e09acd
P-A.Blanc <utu.ubba@gmail.com>
2017-07-19 14:35:03 +0200

    Update with correct interface

--- src/js/app-form.js
+++ src/js/app-form.js
@@ -2,16 +2,30 @@ export default {
   name: 'app-form',
   data: function () {
     return {
-      title : "Form",
+      titles : ["Search", "Meta data", "Personalize"],
+      currentTitle: "Search",
+      clicks: 0,
+      searchActif: true,
+      metaDataActif: false,
+      personalizeActif: false,
+      buttonText: ["next", "next", "generate"],
+      currentButtonText: "Next",
       websiteValue: "www.example.com",
+      issuerNameValue: "",
+      receiverNameValue: "",
       emailValue: "example@gmail.com",
       searchValue: "",
       searchResults: [],
       descriptionValue : "Got it for feeding cats",
       badge:{},
+      imageInputActif: true,
+      companyNameInput: true,
+      urlInput: true,
+      colorInput: true,
       nameBadge: "round_language",
       firstHref:"",
-      count:0
+      count:0,
+      language: "en"
      }
    },
   methods: {
@@ -25,19 +39,19 @@ export default {
             var res=result.title;
             console.log(res);
             if(res==="social interaction"){
-              changeName("hexagon_social");
+              changeName("decagon_social");
             }
             else if(res==="language"){
-              changeName("hexagon_language");
+              changeName("decagon_language");
             }
             else if(res==="thinking"){
-              changeName("hexagon_thinking");
+              changeName("decagon_thinking");
             }
             else if(res==="attitudes and values"){
-              changeName("hexagon_attitudes");
+              changeName("decagon_attitudes");
             }
             else if(res=="application of knowledge"){
-              changeName("hexagon_applications");
+              changeName("decagon_applications");
             }
             else if (result._links.broaderSkillGroup){
               broadestConcept(result._links.broaderSkillGroup[0].href, changeName);
@@ -58,14 +72,65 @@ export default {
       broadestConcept(href, nameChange);
     },
     submit: function (events) {
-      console.log("submitted");
-      var href=this.firstHref;
-      this.getCorrectTag(href, function (x){
-        this.nameBadge=x;
-        document.getElementById(this.nameBadge).style.visibility="visible";
-      }.bind(this));
+      if(this.clicks < 3) {
+        this.clicks += 1;
+        this.currentTitle = this.titles[this.clicks];
+        this.currentButtonText = this.buttonText[this.clicks];
+        if(this.clicks == 1) {
+          var href=this.firstHref;
+          this.getCorrectTag(href, function (x){
+            this.nameBadge=x;
+            document.getElementById("svgFile").style.visibility="visible"
+            document.getElementById(this.nameBadge).style.visibility="visible";
+          }.bind(this));
+          this.searchActif = !this.searchActif;
+          this.metaDataActif = !this.metaDataActif;
+        }else if (this.clicks == 2) {
+          this.metaDataActif = !this.metaDataActif;
+          this.personalizeActif = !this.personalizeActif;
+        }else{
+          this.clicks-=1;
+          console.log("generation call");
+          this.generation().bind(this);
+        }
+      } else {
+        console.log("error in next");
+      }
+    },
+
+    back: function () {
+      if(this.clicks < 3 && this.clicks > 0) {
+        this.clicks -= 1;
+        this.currentTitle = this.titles[this.clicks];
+        this.currentButtonText = this.buttonText[this.clicks];
+        if (this.clicks == 0) {
+          this.searchActif = !this.searchActif;
+          this.metaDataActif = !this.metaDataActif;
+        }else if(this.clicks == 1) {
+          this.metaDataActif = !this.metaDataActif;
+          this.personalizeActif = !this.personalizeActif;
+        }else if (this.clicks == 2) {
+          this.metaDataActif = !this.metaDataActif;
+          this.personalizeActif = !this.personalizeActif;
+        }
+      }
+    },
+    changeStateInputField: function (input) {
+      console.log(input)
+      if(input === "imageInputActif") {
+        console.log("hey")
+        console.log("hey")
+        this.imageInputActif = !this.imageInputActif
+      }else if(input === "companyNameInput") {
+        this.companyNameInput = !this.companyNameInput
+      }else if (input === "urlInput") {
+        this.urlInput = !this.urlInput
+      }else {
+        this.colorInput = !this.colorInput
+      }
     },
     generation: function (event){
+      console.log("generation");
       function signingComplete(){
         var encoder = new TextEncoder('utf-8');
         function keyGeneration(resolve, reject){
@@ -111,11 +176,10 @@ export default {
       //might require the client to do so.
       var d = new Date();
       var date = d.toISOString();
-      //var s = new XMLSerializer().serializeToString(document.getElementById("svgFile"))
+      var s = new XMLSerializer().serializeToString(document.getElementById("svgFile"))
       var encodedData="";
-      //var encodedData = window.btoa(unescape(encodeURIComponent(s)));
+      var encodedData = window.btoa(unescape(encodeURIComponent(s)));
       var imageURI = "data:image/svg+xml;base64,"+encodedData;
-      console.log(imageURI);
       function ab2str(buf) {
         return String.fromCharCode.apply(null, new Uint16Array(buf));
       }
@@ -131,54 +195,54 @@ export default {
       }
       var buf =str2ab(this.emailValue);
       crypto.subtle.digest("SHA-256", buf).then(function(resultHash){
-      var resHash=ab2str(resultHash);
-      console.log(resHash);
-      fetch(href, {
+        var resHash=ab2str(resultHash);
+        fetch(href, {
         method: "get"
-      }).then(r => r.json())
-      .then(result => {
-        if(result){
-              var referenceURI = (''+href.match("uri=.*?&"));
-              var descriptionSkill = result.description.en.literal;
-              var title = result.title;
-              this.badge = {id : assertionVar,
-                type : "Assertion",
-                recipient : {
-                  type : "email",
-                  identity : resHash
+        }).then(r => r.json())
+        .then(result => {
+          if(result){
+            var referenceURI = (''+href.match("uri=.*?&"));
+            var descriptionSkill = result.description.en.literal;
+            var title = result.title;
+            this.badge = {id : assertionVar,
+              type : "Assertion",
+              recipient : {
+                type : "email",
+                identity : resHash
+              },
+              issuedOn : date,
+              verification : {
+                type : "signed"
+              },
+              badge: {
+                id : this.websiteValue+" "+title,
+                type : "BadgeClass",
+                name : title,
+                description : descriptionSkill+" ESCO database URI : "+referenceURI.substring(4, referenceURI.length-1),
+                image : imageURI,
+                criteria : {
+                  narrative : this.descriptionValue
                 },
-                issuedOn : date,
-                verification : {
-                  type : "signed"
-                },
-                badge: {
-                  id : this.websiteValue+" "+title,
-                  type : "BadgeClass",
-                  name : title,
-                  description : descriptionSkill+" ESCO database URI : "+referenceURI.substring(4, referenceURI.length-1),
-                  image : imageURI,
-                  criteria : {
-                    narrative : this.descriptionValue
-                  },
-                  issuer : {
-                    id : this.websiteValue,
-                    type : "Issuer",
-                    name : this.websiteValue,
-                    url : this.websiteValue,
-                    email : this.websiteValue
-                  }
+                issuer : {
+                  id : this.websiteValue,
+                  type : "Issuer",
+                  name : this.websiteValue,
+                  url : this.websiteValue,
+                  email : this.websiteValue
                 }
-              };
-              var filesave = require('./FilaSaver.js');
-              var data = "data:json;charset=utf-8,";
-              var blob = new Blob([JSON.stringify(this.badge)], {type: data});
-              filesave.saveAs(blob, "badge.json");
-        }
-      });
-}.bind(this))
+              }
+            };
+            var filesave = require('./FilaSaver.js');
+            var data = "data:json;charset=utf-8,";
+            var blob = new Blob([JSON.stringify(this.badge)], {type: data});
+            filesave.saveAs(blob, "badge.json");
+          }
+        });
+      }.bind(this))
     },
 
     onChangeSearch: function () {
+      document.getElementById("svgFile").style.visibility="hidden"
       document.getElementById(this.nameBadge).style.visibility="hidden"
       let list = document.querySelector('.list');
       while (list.firstChild) {
@@ -230,7 +294,7 @@ export default {
           document.getElementById("text1").textContent = string1;
           document.getElementById("text2").textContent = string2;
           document.getElementById("text3").textContent = string3;
-        let fetched = fetch(`http://esp-api-dev-0.10.0.cogni.zone/search?text=${this.searchValue}&language=en&type=skill&facet=type&facet=isInScheme`, {
+        let fetched = fetch(`http://esp-api-dev-0.10.0.cogni.zone/search?text=${this.searchValue}&language=`+this.language+`&type=skill&facet=type&facet=isInScheme`, {
   	       method: 'get'
         })
 
@@ -253,6 +317,51 @@ export default {
     },
 
     handleImage: function () {
+      function getAverageColourAsRGB (img) {
+        //https://gist.github.com/olvado/1048628
+        //some modification to be up to date and make the color less agressives.
+        var canvas = document.createElement('canvas'),
+        context = canvas.getContext("2d"),
+        rgb = {r:102,g:102,b:102}, // Set a base colour as a fallback for non-compliant browsers
+        pixelInterval = 5, // Rather than inspect every single pixel in the image inspect every 5th pixel
+        count = 0,
+        i = -4,
+        data, length;
+        // return the base colour for non-compliant browsers
+        if (!context) { return rgb; }
+        // set the height and width of the canvas element to that of the image
+        var height = canvas.height = img.naturalHeight || img.offsetHeight || img.height,
+        width = canvas.width = img.naturalWidth || img.offsetWidth || img.width;
+
+        context.drawImage(img, 0, 0);
+
+        try {
+          data = context.getImageData(0, 0, width, height);
+        } catch(e) {
+        // catch errors - usually due to cross domain security issues
+          alert(e);
+          return rgb;
+        }
+
+        data = data.data;
+        length = data.length;
+        while ((i += pixelInterval * 4) < length) {
+          count++;
+          rgb.r += data[i];
+          rgb.g += data[i+1];
+          rgb.b += data[i+2];
+        }
+  
+        // floor the average values to give correct rgb values (ie: round number values)
+        rgb.r = rgb.r/count;
+        rgb.g = rgb.g/count;
+        rgb.b = rgb.b/count;
+        var res = (rgb.r+rgb.g+rgb.b - 127*3)/6;
+        rgb.r=Math.floor(rgb.r-res);
+        rgb.b=Math.floor(rgb.b-res);
+        rgb.g=Math.floor(rgb.g-res);
+        return rgb;
+      }
       let input = document.getElementById('image-input');
       let file  = input.files[0];
       var img = new Image();
@@ -265,11 +374,9 @@ export default {
         };
         if(sizes.width < 480 && sizes.height < 150){
           let confirmBox = confirm("Are you sure you want to continue?")
-          if(confirmBox === true) {
-            this.displayImage(file, img);
+          if(confirmBox != true) {
+            console.log("todo");
           }
-        }else {
-            this.displayImage(file, img);
         }
         img.height=150;
         let stuff=document.getElementById("logo");
@@ -277,20 +384,17 @@ export default {
         reader.onload = (function(aImg) {
           return function(e) {
             document.getElementById("logo").setAttributeNS('http://www.w3.org/1999/xlink', 'xlink:href', e.target.result);
-            console.log(document.getElementById("logo"));
+            document.getElementById("logo").style.visibility="visible";
+            var rgb=getAverageColourAsRGB(img);
+            var rgbString="rgb("+rgb.r+", "+rgb.g+", "+rgb.b+")"
+            var x= document.getElementsByClassName("st0");
+            for(var i=0; i<x.length; i++){
+              x[i].style.fill=rgbString;
+            };
           }.bind(this); 
         }.bind(this))(stuff);
         reader.readAsDataURL(file);
       };
-    },
-
-    displayImage: function (file, img) {
-      let image = document.getElementById('image');
-      image.file = file;
-      let reader = new FileReader();
-      reader.onload = (function(aImg) { return function(e) { aImg.src = e.target.result; }; })(image);
-      reader.readAsDataURL(file);
     }
   }
-
 }
